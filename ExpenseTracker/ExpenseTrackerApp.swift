0f4460f47cf1a87b35916d9ed06a07a1c5512c4b
Patricia Ong <patricia.ong977@gmail.com>
2022-06-08 18:22:34 -0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Patricia Ong on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
