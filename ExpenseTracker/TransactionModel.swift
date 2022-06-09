4d9726a8f349c6759d2937cb793e6ad8b0024e92
Patricia Ong <patricia.ong977@gmail.com>
2022-06-08 20:54:33 -0500

    Re-directed TransactionModel file. Added an enum struct for TransactionTypes.

--- ExpenseTracker/TransactionModel.swift
+++ ExpenseTracker/TransactionModel.swift
@@ -9,17 +9,25 @@ import Foundation
 
 struct Transaction: Identifiable { // Identifiable - automatically use the property.
     // Each item in this list is unique.
+    // Let - constant/read-only, var - editable.
     let id: Int
     let date: String
     let institution: String
     let account: String
     let amount: Double
-    let type: TransactionType.RawValue //
+    let type: TransactionType.RawValue // The string version.
     var categoryId: Int
     var category: String
     let isPending: Bool
     var isTransfer: Bool
     var isExpense: Bool
     var isEdited: Bool
+}
+
+// Create a user-defined data type that has fixed values.
+// Enum - enumeration
+enum TransactionType : String { // return a String
+    case debit = "debit"
+    case credit = "credit"
     
 }
